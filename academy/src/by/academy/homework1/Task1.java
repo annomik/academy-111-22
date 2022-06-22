11741d85bece7a612bf492d2a9af4893d9f7a924
Hanna Mikulich <mikulichhanna@gmail.com>
2022-06-22 18:07:26 +0300

    Second commit. Refresh

--- academy/src/by/academy/homework1/Task1.java
+++ academy/src/by/academy/homework1/Task1.java
@@ -11,37 +11,37 @@ public class Task1 {
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Введите сумму покупки:");
+		System.out.println("Р’РІРµРґРёС‚Рµ СЃСѓРјРјСѓ РїРѕРєСѓРїРєРё:");
 
 		if (sc.hasNextDouble() ) {
 			sum = sc.nextDouble();	
-			System.out.println("Введите возраст покупателя:");
+			System.out.println("Р’РІРµРґРёС‚Рµ РІРѕР·СЂР°СЃС‚ РїРѕРєСѓРїР°С‚РµР»СЏ:");
 
 			if (sc.hasNextInt()) {
 				age = sc.nextInt();
 
 				if (sum >= 400)
-					System.out.println("Цена: "+sum*0.8+ " руб. Ваша скидка 20%");
+					System.out.println("пїЅпїЅпїЅпїЅ: "+sum*0.8+ " пїЅпїЅпїЅ. пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ 20%");
 				else 
 					if (sum >= 300)
-						System.out.println("Цена: "+sum*0.85+" руб. Ваша скидка 15%");
+						System.out.println("пїЅпїЅпїЅпїЅ: "+sum*0.85+" пїЅпїЅпїЅ. пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ 15%");
 					else
 						if (sum >= 200 && age>18)
-							System.out.println("Цена: "+sum*0.84+" руб. Ваша скидка 16%");
+							System.out.println("пїЅпїЅпїЅпїЅ: "+sum*0.84+" пїЅпїЅпїЅ. пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ 16%");
 						else 
 							if (sum >= 200 && age<=18)
-								System.out.println("Цена: "+sum*0.91+" руб. Ваша скидка 9%");
+								System.out.println("пїЅпїЅпїЅпїЅ: "+sum*0.91+" пїЅпїЅпїЅ. пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ 9%");
 							else
 								if (sum >= 100)
-									System.out.println("Цена: "+sum*0.93+" руб. Ваша скидка 7%");
+									System.out.println("пїЅпїЅпїЅпїЅ: "+sum*0.93+" пїЅпїЅпїЅ. пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ 7%");
 								else
-									System.out.println("Цена: "+sum*0.95+" руб. Ваша скидка 5%");
+									System.out.println("пїЅпїЅпїЅпїЅ: "+sum*0.95+" пїЅпїЅпїЅ. пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ 5%");
 			}
 			else 
-				System.out.println("Некорректные данные!");
+				System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ!");
 		}
 		else 
-			System.out.println("Некорректные данные!");
+			System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ!");
 
 		sc.close();
 
